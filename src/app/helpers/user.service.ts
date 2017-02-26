5523e3839c3539e418f583f19de72cbb94597856
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-02-25 19:06:38 -0500

    Backend handling of signup complete

--- src/app/helpers/user.service.ts
+++ src/app/helpers/user.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http,Headers,RequestOptions,Response } from '@angular/http';
+import { Signup } from 'app/signup/signup';
+import 'rxjs/Rx';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  constructor(
+	  private http:Http
+  ) { }
 
+  signup(signup:Signup):Observable<boolean>{
+		console.debug("posting signup to server");
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+		let options=new RequestOptions({headers:headers});
+		return this.http.post("/api/create-user",JSON.stringify(signup),options).map((res:Response)=>{return res.json()});
+	}
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Signup } from './signup';
-
+import { UserService } from '../helpers/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,10 +10,34 @@ import { Signup } from './signup';
 export class SignupComponent implements OnInit {
 
 	private signup:Signup=new Signup();
+	private failureMessage:string;
 
-	constructor() { }
+	constructor(
+		private router:Router,
+		private userService:UserService
+		) { }
 
 	ngOnInit() {
+	}
+
+	doSignup(){
+		// call the service
+		console.debug("Signup a new user");
+		this.userService.signup(this.signup).subscribe((pass:boolean)=>{
+			console.debug("moving to homepage");
+			this.router.navigate(["/"]);
+		},(error:any)=>{
+			if(error.status==422){
+				if(error._body==2){
+					this.failureMessage="Email already exists";
+				}else if(error._body==3){
+					this.failureMessage="Weak password";
+				}else if(error._body==4){
+					this.failureMessage="Null password";
+				}
+			}
+			console.debug("error "+error);
+		});
 	}
 
 }
