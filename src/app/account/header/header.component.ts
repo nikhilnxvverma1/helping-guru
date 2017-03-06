e39566b7ed2400851e51e38cc1340ab102f5536c
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-03-05 21:17:42 -0500

    User data binding in header with dummy values.

--- src/app/account/header/header.component.ts
+++ src/app/account/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+	@Input() private user:User;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+		this.user=User.dummy();//TODO testing
+	}
 
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -12,4 +12,18 @@ export class User{
 	fullPhotoUrl:string;
 	points:number;
 	projectList:Project[]=[]
+
+	static dummy():User{
+		let user=new User();
+		user.firstName="Nikhil";
+		user.lastName="Verma";
+		user.email="nikhilverma@madebynikhil.com";
+		user.password="asdf";
+		user.gender="Male";
+		user.thumbnailUrl="https://robohash.org/aliasdistinctionecessitatibus.png?size=50x50&set=set1";
+		user.dateOfBirth=new Date(1991,2,28);
+		user.fullPhotoUrl="http://dummyimage.com/205x241.png/ff4444/ffffff";
+		user.points=234;
+		return user;
+	}
 }
